16c23c6bfc3b39f3233b233374f58b01e9f089f8
elmasbusenur@gmail.com <elmasbusenur@gmail.com>
2020-08-09 15:26:21 +0300

    admin panel oluşturulması, adminde login olunması, super kullanıcı, tablonun adminde görünmesi

--- ilan/admin.py
+++ ilan/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from ilan.models import Category
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']  # hangi alanlar görülsün istiyorum categoride
+    #readonly_fields = ('image_tag',)
+    list_filter = ['status']  # statuye göre filtreliyorum
+admin.site.register(Category, CategoryAdmin) #category bir model ve import ediyoruz
